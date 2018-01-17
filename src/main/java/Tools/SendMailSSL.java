e9bb926ad4959c29c4315cb2f2a0b24db86258b7
Sergiy1987 <nedved198725@gmail.com>
2018-01-17 17:31:01 +0200

    Test Cases for UkrNet

--- src/main/java/Tools/SendMailSSL.java
+++ src/main/java/Tools/SendMailSSL.java
@@ -1,7 +1,11 @@
 package Tools;
+
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.util.Date;
 import java.util.Properties;
 public class SendMailSSL {
     public static void send(String login, String password, String to, String subject, String text) {
@@ -11,7 +15,7 @@ public class SendMailSSL {
         props.put("mail.smtp.socketFactory.class",
                 "javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.port", "465");//465
         Session session = Session.getDefaultInstance(props,
                 new javax.mail.Authenticator() {
                     protected PasswordAuthentication getPasswordAuthentication() {
@@ -20,13 +24,34 @@ public class SendMailSSL {
                 });
         try {
             Message message = new MimeMessage(session);
+            Multipart multipart = new MimeMultipart();
+            MimeBodyPart htmlPart = new MimeBodyPart();
+            MimeBodyPart txtPart = new MimeBodyPart();
+
             message.setFrom(new InternetAddress(login));
             message.setRecipients(Message.RecipientType.TO,
                     InternetAddress.parse(to));
             message.setSubject(subject);
-            message.setText(text);
+
+            String htmlContent = "<html><body><font color=red>Greetings!</font><br>";
+            htmlContent += "<b>Test Passed successfully!</b><br>";
+            htmlContent += "From: "+ login + "<br>"
+                            + "To: " + to + "<br>"
+                            + "Subject: " + subject + "<br>"
+                            + "Data: " + new Date() + "<br>"
+                            + "<b>Result from DataBase: </b>"
+                            + "</body></html>";
+
+            htmlPart.setContent(htmlContent, "text/html");
+            multipart.addBodyPart(htmlPart);
+            message.setContent(multipart);
+
+            txtPart.setText(text);
+            multipart.addBodyPart(txtPart);
+
+           // message.setText(text);
             Transport.send(message);
-            System.out.println("Done");
+            System.out.println("Sent message successfully...");
         } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
