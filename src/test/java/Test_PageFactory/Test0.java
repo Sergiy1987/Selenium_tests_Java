4e8b4b377e4cb519b1005249dd9a83ff8a1b610f
Sergiy1987 <nedved198725@gmail.com>
2018-01-30 15:08:12 +0200

    Test Cases for UkrNet

--- src/test/java/Test_PageFactory/Test0.java
+++ src/test/java/Test_PageFactory/Test0.java
@@ -7,7 +7,6 @@ import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import org.testng.annotations.Listeners;
-import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -27,7 +26,7 @@ public class Test0 extends MainTest {
     @Description("Test Description: Login test with correct Parameters Login and Password.")
     @Story("Verify Login and Password with Parameters({\"USER\",\"PASSWORD\"})")
     @Parameters({ "LOGIN", "PASSWORD" })
-public void LoginWithParameters(@Optional ("LOGIN") String LOGIN, @Optional ("PASSWORD") String PASSWORD) {
+public void LoginWithParameters(String LOGIN,String PASSWORD) {
         test = createTest("HomePage", "LoginWithParameters");
         if (wait.elementDisplayedByWebElement(LOGIN_BUTTON)) {
             homePage.LoginParameters(LOGIN, PASSWORD);
